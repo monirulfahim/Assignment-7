6a1dc5de28c69fa679df3f6c1174deac476b79b3
monirulfahim <monirulfahim65@gmail.com>
2026-04-16 15:22:51 +0600

    add Timeline view with icons and hide banner/friends sections

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import friendsData from './friends.json';
 import './App.css'
 import { useState } from 'react';
 import { useEffect } from 'react';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
 
@@ -9,6 +11,35 @@ function App() {
   const [view, setView] = useState("Home");
   const [loading, setLoading] = useState(true);
   const [selectedFriend, setSelectedFriend] = useState(null);
+  const [timeline, setTimeline] = useState([]);
+
+  const handleAction = (type) => {
+    let icon;
+
+    if (type === "Call") icon = <img src="images/call.png" alt="" srcset="" />;
+    if (type === "Text") icon = <img src="images/text.png" alt="" srcset="" />;
+    if (type === "Video") icon = <img src="images/video.png" alt="" srcset="" />;
+
+    const newEvent = {
+      id: Date.now(),
+      name: selectedFriend.name,
+      type: type,
+      icon: icon,
+      date: new Date().toLocaleDateString()
+    };
+
+    setTimeline((prev) => [newEvent, ...prev]);
+
+    toast(
+      <div className="flex items-center gap-2 text-xl">
+        {icon} {type} with {selectedFriend.name}
+      </div>,
+      {
+        position: "top-center",
+        autoClose: 2000,
+      }
+    );
+  };
 
   useEffect(() => {
     setTimeout(() => {
@@ -31,7 +62,10 @@ function App() {
                     `}><i className="fa-regular fa-house"></i> Home</button>
           </div>
           <div>
-            <button onClick={() => setView("Timeline")} className={`rounded-sm px-2.5 py-1.5  cursor-pointer max-[768px]:px-2
+            <button onClick={() => {
+              setView("Timeline");
+              setSelectedFriend(null);
+            }} className={`rounded-sm px-2.5 py-1.5  cursor-pointer max-[768px]:px-2
                     ${view === "Timeline" ? "bg-[#244D3F] text-[#FFFFFF] rounded-sm px-2.5 py-1.5  cursor-pointer max-[768px]:px-2" : "text-[#244D3F] bg-[#FFFFFF] rounded-sm px-2.5 py-1.5  cursor-pointer max-[768px]:px-2"}`}><i className="fa-regular fa-clock"></i> Timeline</button>
           </div>
           <div>
@@ -85,42 +119,65 @@ function App() {
         </div>
       </section>
 
+      {view === "Timeline" && (
+        <section className="p-10 bg-[#F8FAFC] max-[576px]:p-4">
+          <h1 className="text-2xl mb-4">Timeline</h1>
+
+          {timeline.map((item) => (
+            <div key={item.id} className="bg-[#FFFFFF] p-4 mb-2 rounded-lg shadow flex gap-4 items-center max-[576px]:p-3 max-[576px]:gap-2">
+              <div className="font-semibold">
+                <div className='text-xs'> {item.icon} </div>
+              </div>
+              <div>
+                <div className='flex gap-2.5 text-xl font-semibold mb-2.5 max-[576px]:gap-1.5'>
+                  <div> {item.type} </div> 
+                 <div>  With {item.name} </div>
+                </div>
+                <div>
+                  <p className="text-gray-500 text-xl">{item.date}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </section>
+      )}
+
 
       {/* Banner Section */}
 
-      {!selectedFriend && (
+      {!selectedFriend && view !== "Timeline" && (
         <section className='bg-[#F8FAFC] pl-50 pr-50 pt-20 pb-18 max-[576px]:pt-10 max-[576px]:pr-6 max-[576px]:pb-6 max-[576px]:pl-6 text-center max-[768px]:pt-10 max-[768px]:pr-6 max-[768px]:pb-6 max-[768px]:pl-6'>
-        <div className='flex flex-col justify-center items-center mb-11'>
-          <h1 className='text-5xl text-[#1F2937] font-bold mb-4 max-[576px]:text-2xl'>Friends to keep close in your life</h1>
-          <p className='text-[#64748B] mb-8 text-center'>
-            Your personal shelf of meaningful connections. Browse, tend, and nurture the <br /> relationships that matter most.
-          </p>
-          <button className='bg-[#244D3F] text-[#FFFFFF] rounded-sm px-2.5 py-1.5 pointer-cursor cursor-pointer'><i className="fa-solid fa-plus"></i> Add a Friend</button>
-        </div>
-        <div className='flex justify-center gap-9 items-center max-[576px]:flex-col max-[576px]:gap-6 max-[576px]:mt-10 max-[768px]:gap-6'>
-          <div className='bg-[#FFFFFF] text-center px-12 py-8 rounded-lg shadow-sm max-[768px]:px-7 max-[768px]:py-4'>
-            <h3 className='text-3xl text-[#244D3F] font-semibold mb-0.5'>8</h3>
-            <p className='text-[#64748B]'>Total Friends</p>
+          <div className='flex flex-col justify-center items-center mb-11'>
+            <h1 className='text-5xl text-[#1F2937] font-bold mb-4 max-[576px]:text-2xl'>Friends to keep close in your life</h1>
+            <p className='text-[#64748B] mb-8 text-center'>
+              Your personal shelf of meaningful connections. Browse, tend, and nurture the <br /> relationships that matter most.
+            </p>
+            <button className='bg-[#244D3F] text-[#FFFFFF] rounded-sm px-2.5 py-1.5 pointer-cursor cursor-pointer'><i className="fa-solid fa-plus"></i> Add a Friend</button>
           </div>
-          <div className='bg-[#FFFFFF] text-center px-16 py-8 rounded-lg shadow-sm max-[768px]:px-10 max-[768px]:py-4'>
-            <h3 className='text-3xl text-[#244D3F] font-semibold mb-0.5'>3</h3>
-            <p className='text-[#64748B]'>On Track</p>
+          <div className='flex justify-center gap-9 items-center max-[576px]:flex-col max-[576px]:gap-6 max-[576px]:mt-10 max-[768px]:gap-6'>
+            <div className='bg-[#FFFFFF] text-center px-12 py-8 rounded-lg shadow-sm max-[768px]:px-7 max-[768px]:py-4'>
+              <h3 className='text-3xl text-[#244D3F] font-semibold mb-0.5'>8</h3>
+              <p className='text-[#64748B]'>Total Friends</p>
+            </div>
+            <div className='bg-[#FFFFFF] text-center px-16 py-8 rounded-lg shadow-sm max-[768px]:px-10 max-[768px]:py-4'>
+              <h3 className='text-3xl text-[#244D3F] font-semibold mb-0.5'>3</h3>
+              <p className='text-[#64748B]'>On Track</p>
+            </div>
+            <div className='bg-[#FFFFFF] text-center px-11 py-8 rounded-lg shadow-sm max-[768px]:px-6 max-[768px]:py-4'>
+              <h3 className='text-3xl text-[#244D3F] font-semibold mb-0.5'>4</h3>
+              <p className='text-[#64748B]'>Need Attention</p>
+            </div>
+            <div className='bg-[#FFFFFF] text-center px-5 py-8 rounded-lg shadow-sm max-[576px]:px-1.5 max-[768px]:px-3 max-[768px]:py-4'>
+              <h3 className='text-3xl text-[#244D3F] font-semibold mb-0.5'>12</h3>
+              <p className='text-[#64748B]'>Interactions This Month</p>
+            </div>
           </div>
-          <div className='bg-[#FFFFFF] text-center px-11 py-8 rounded-lg shadow-sm max-[768px]:px-6 max-[768px]:py-4'>
-            <h3 className='text-3xl text-[#244D3F] font-semibold mb-0.5'>4</h3>
-            <p className='text-[#64748B]'>Need Attention</p>
-          </div>
-          <div className='bg-[#FFFFFF] text-center px-5 py-8 rounded-lg shadow-sm max-[576px]:px-1.5 max-[768px]:px-3 max-[768px]:py-4'>
-            <h3 className='text-3xl text-[#244D3F] font-semibold mb-0.5'>12</h3>
-            <p className='text-[#64748B]'>Interactions This Month</p>
-          </div>
-        </div>
-      </section>
+        </section>
       )}
 
       {/* Friends card Section */}
 
-      {!selectedFriend && (
+      {!selectedFriend && view !== "Timeline" && (
         <section className='pl-45 pr-45 pt-5 pb-20 bg-[#F8FAFC] max-[576px]:pt-10 max-[576px]:pr-6 max-[576px]:pb-6 max-[576px]:pl-6 max-[768px]:pt-10 max-[768px]:pr-6 max-[768px]:pb-6 max-[768px]:pl-6'>
           <div className='text-3xl font-semibold mb-5'>
             <h1>My Friends</h1>
@@ -160,7 +217,7 @@ function App() {
       )}
 
       {selectedFriend && (
-        <section className='bg-[#f5f4f4] pl-57 pr-57 pt-20 pb-20 flex gap-6 items-center'>
+        <section className='bg-[#f5f4f4] pl-57 pr-57 pt-20 pb-20 flex gap-6 items-center max-[576px]:pt-10 max-[576px]:pr-2 max-[576px]:pb-6 max-[576px]:pl-2 max-[576px]:flex-col max-[768px]:pt-10 max-[768px]:pr-4 max-[768px]:pb-6 max-[768px]:pl-4 max-[768px]:flex-col'>
           <div>
             <div>
               <div key={selectedFriend.id} className='flex flex-col gap-3 justify-center items-center p-8 rounded-lg bg-[#FFFFFF] cursor-pointer mb-4'>
@@ -192,56 +249,59 @@ function App() {
             </div>
             <div className='text-center'>
               <div className='bg-[#FFFFFF] text-[#1F2937] p-4 mb-2 font-semibold rounded-lg'>
-                <span><i class="fa-solid fa-alarm-clock"></i> Snooze 2 weeks</span>
+                <span><i className="fa-solid fa-alarm-clock"></i> Snooze 2 weeks</span>
               </div>
               <div className='bg-[#FFFFFF] text-[#1F2937] p-4 mb-2 font-semibold rounded-lg'>
-                <span><i class="fa-solid fa-box-archive"></i> Archive</span>
+                <span><i className="fa-solid fa-box-archive"></i> Archive</span>
               </div>
               <div className='bg-[#FFFFFF] text-[#EF4444] p-4 font-semibold rounded-lg'>
-                <span><i class="fa-solid fa-trash-can"></i> Delete</span>
+                <span><i className="fa-solid fa-trash-can"></i> Delete</span>
               </div>
             </div>
           </div>
           <div>
-            <div className='flex gap-6 justify-between items-center'> 
-                <div className='bg-[#FFFFFF] pl-10 pr-10 pt-8 pb-8 text-center rounded-lg'> 
-                  <h1 className='text-[#244D3F] text-2xl font-semibold'>{selectedFriend.contact}</h1>
-                  <p className='text-[#64748B]'>Days Since Contact</p>
-                </div>
-                <div className='bg-[#FFFFFF] pl-12 pr-12 pt-8 pb-8 text-center rounded-lg'> 
-                  <h1 className='text-[#244D3F] text-2xl font-semibold'>30</h1>
-                  <p className='text-[#64748B]'>Goal (Days)</p>
-                </div>
-                <div className='bg-[#FFFFFF] pl-7 pr-7 pt-8 pb-8 text-center rounded-lg'> 
-                  <h1 className='text-[#244D3F] text-2xl font-semibold wrap-anywhere'>Feb 27, 2026</h1>
-                  <p className='text-[#64748B]'>Next Due</p>
-                </div>
+            <div className='flex gap-6 justify-between items-center max-[576px]:gap-3  max-[576px]:justify-center  max-[576px]:flex-col max-[768px]:gap-3'>
+              <div className='bg-[#FFFFFF] pl-10 pr-10 pt-8 pb-8 text-center rounded-lg'>
+                <h1 className='text-[#244D3F] text-2xl font-semibold'>{selectedFriend.contact}</h1>
+                <p className='text-[#64748B]'>Days Since Contact</p>
+              </div>
+              <div className='bg-[#FFFFFF] pl-12 pr-12 pt-8 pb-8 text-center rounded-lg  max-[576px]:pl-16  max-[576px]:pr-16'>
+                <h1 className='text-[#244D3F] text-2xl font-semibold'>30</h1>
+                <p className='text-[#64748B]'>Goal (Days)</p>
+              </div>
+              <div className='bg-[#FFFFFF] pl-7 pr-7 pt-8 pb-8 text-center rounded-lg  max-[576px]:pl-8  max-[576px]:pr-8'>
+                <h1 className='text-[#244D3F] text-2xl font-semibold wrap-anywhere'>Feb 27, 2026</h1>
+                <p className='text-[#64748B]'>Next Due</p>
+              </div>
             </div>
-            <div className='p-6 bg-[#FFFFFF] mt-6 mb-6 rounded-lg'> 
-                <div className='flex justify-between items-center mb-1'> 
-                  <div><p className='text-xl'>Relationship Goal</p></div>
-                  <div className='bg-[#E9E9E9] border border-[#E9E9E9] text-[#1F2937] rounded-sm font-semibold p-2'>Edit</div>
-                </div>
-                <div> 
-                  <p><span className='text-[#64748B] text-xl'>Connect every</span> <span className='text-[#1F2937] font-bold text-xl'>30 days</span></p>
-                </div>
+            <div className='p-6 bg-[#FFFFFF] mt-6 mb-6 rounded-lg  max-[576px]:p-3'>
+              <div className='flex justify-between items-center mb-1 max-[576px]:justify-center  max-[576px]:gap-20'>
+                <div><p className='text-xl'>Relationship Goal</p></div>
+                <div className='bg-[#E9E9E9] border border-[#E9E9E9] text-[#1F2937] rounded-sm font-semibold p-2'>Edit</div>
+              </div>
+              <div>
+                <p><span className='text-[#64748B] text-xl'>Connect every</span> <span className='text-[#1F2937] font-bold text-xl'>30 days</span></p>
+              </div>
             </div>
-            <div className='bg-[#FFFFFF] p-6 rounded-lg'> 
-                  <h2 className='text-xl mb-4'>Quick Check-In</h2>
-                  <div className='flex gap-7 items-center'> 
-                    <div className='bg-[#F8FAFC] pl-18 pr-18 pt-5 pb-5 cursor-pointer rounded-lg text-center '> 
-                      <p><i className="fa-solid fa-phone text-xl"></i></p>
-                      <p className='text-xl font-semibold'>Call</p>
-                    </div>
-                    <div className='bg-[#F8FAFC] pl-18 pr-18 pt-5 pb-5 cursor-pointer rounded-lg text-center'> 
-                      <p><i className="fa-regular fa-comment-dots text-xl"></i></p>
-                      <p className='text-xl font-semibold'>Text</p>
-                    </div>
-                    <div className='bg-[#F8FAFC] pl-18 pr-18 pt-5 pb-5 cursor-pointer rounded-lg text-center'> 
-                      <p><i className="fa-solid fa-video text-xl"></i></p>
-                      <p className='text-xl font-semibold'>Video</p>
-                    </div>
-                  </div>
+            <div className='bg-[#FFFFFF] p-6 rounded-lg'>
+              <h2 className='text-xl mb-4  max-[576px]:text-center'>Quick Check-In</h2>
+              <div className='flex gap-7 items-center  max-[576px]:flex-col'>
+                <div onClick={() => handleAction("Call")
+                } className='bg-[#F8FAFC] pl-18 pr-18 pt-5 pb-5 cursor-pointer rounded-lg text-center '>
+                  <p><i className="fa-solid fa-phone text-xl"></i></p>
+                  <p className='text-xl font-semibold'>Call</p>
+                </div>
+                <div onClick={() => handleAction("Text")
+                } className='bg-[#F8FAFC] pl-18 pr-18 pt-5 pb-5 cursor-pointer rounded-lg text-center'>
+                  <p><i className="fa-regular fa-comment-dots text-xl"></i></p>
+                  <p className='text-xl font-semibold'>Text</p>
+                </div>
+                <div onClick={() => handleAction("Video")
+                } className='bg-[#F8FAFC] pl-18 pr-18 pt-5 pb-5 cursor-pointer rounded-lg text-center'>
+                  <p><i className="fa-solid fa-video text-xl"></i></p>
+                  <p className='text-xl font-semibold'>Video</p>
+                </div>
+              </div>
             </div>
           </div>
         </section>
@@ -283,6 +343,7 @@ function App() {
           </div>
         </div>
       </section>
+      <ToastContainer />
     </>
   )
 }
